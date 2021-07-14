47a58b71693960bdf4d15ca2df73efb244d414a7
Marco Crepaldi <62714235+MarcoCrepaldi-iit@users.noreply.github.com>
2021-07-14 14:40:24 +0200

    Update m.py

--- m.py
+++ m.py
@@ -284,6 +284,7 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
+# taken from GNU Blender: https://svn.blender.org/svnroot/bf-blender/trunk/blender/build_files/scons/tools/bcolors.py
 class bcolors:
     HEADER = '\033[95m'
     OKBLUE = '\033[94m'
